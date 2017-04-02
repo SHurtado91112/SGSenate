541a8871847353201568d449d31f30ce867b2775
SH-Arrow <shurt91112@gmail.com>
2017-04-02 19:41:11 -0400

    4/2/17 screen shot update

--- SGSenate/Util.swift
+++ SGSenate/Util.swift
@@ -25,6 +25,37 @@ class Util: NSObject
     }
 }
 
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
 extension UIColor
 {
     //sg colors
